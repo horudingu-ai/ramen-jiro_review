ce248d8c0348184d8478915d73f6156d817f3c2f
Kouki Segawa <horudingu@gmail.com>
2026-01-13 16:23:09 +0900

    Fix recent review shops grouping for Postgres

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,14 @@ class HomeController < ApplicationController
     .distinct
 
   @pickup_shops = base.to_a.sample(5)
-  
+
    # 新着店舗（最新5件）
     @new_shops = Shop.order(created_at: :desc).limit(5)
 
     # 口コミ新着店舗（最新の口コミから店舗を重複なしで5件）
     @recent_review_shops = Shop
       .joins(:reviews)
-      .includes(:reviews, menu_photos_attachments: :blob)
+      .includes(menu_photos_attachments: :blob)
       .select("shops.*, MAX(reviews.created_at) AS last_reviewed_at")
       .group("shops.id")
       .order(Arel.sql("last_reviewed_at DESC"))
